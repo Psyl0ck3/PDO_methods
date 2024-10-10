15f84da2849164a1f9cd2deefb3bfceb2cb3907f
Glyzza Lei <92197857+Psyl0ck3@users.noreply.github.com>
2024-10-10 14:30:05 +0800

    Add files via upload

---
+++ Bordeos/fetchall.php
@@ -0,0 +1,28 @@
+<?php require_once 'dbConfig.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        
+        #prepare sql for execution
+        $stmt = $pdo->prepare("SELECT * FROM patients");
+
+
+        #retrieves all row from result set 
+        
+        if ($stmt -> execute()) {
+            echo '<pre>';
+             print_r($stmt->fetchAll()); 
+             echo '</pre>';
+              #prints result
+        }
+        
+    ?>
+</body>
+</html>
